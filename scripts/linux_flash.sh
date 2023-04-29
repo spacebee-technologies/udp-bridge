0dbedf18e274f5324de20fce61f057aa24646212
Lautaro Mendieta <54816650+lautaromendieta@users.noreply.github.com>
2023-04-28 22:28:08 -0300

    Fix Linux flash script (#5)

--- scripts/linux_flash.sh
+++ scripts/linux_flash.sh
@@ -3,7 +3,5 @@
 readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
 readonly IMAGE_NAME='espressif/idf:latest'
 
-docker run --rm -v "${PROJECT_DIRECTORY}:/workspace" -v /dev:/dev \
+docker run --privileged --rm -v "${PROJECT_DIRECTORY}:/workspace" -v /dev:/dev \
            -w /workspace "${IMAGE_NAME}" idf.py flash
-docker run --rm -v "${PROJECT_DIRECTORY}:/workspace" -v /dev:/dev \
-           -w /workspace "${IMAGE_NAME}" idf.py flash monitor
