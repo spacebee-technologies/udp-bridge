db9b60dedebdfdd03eab0a2aa5bb144df7a397be
Lautaro Mendieta <lemendieta96@gmail.com>
2023-06-03 11:29:12 -0300

    Remove build and scripts that shall be merged from develop

--- scripts/linux_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
-readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.2.0'
-
-docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
-           -w /workspace  "${IMAGE_NAME}" \
-           bash -c 'arduino-cli compile --fqbn esp32:esp32:esp32 --output-dir build TITO_BRIDGE/'
--- scripts/linux_flash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
-readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.2.0'
-readonly PORT="${1:-/dev/ttyACM0}"
-
-docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
-           -w /workspace -v /dev:/dev "${IMAGE_NAME}" \
-           bash -c "arduino-cli upload -p ${PORT} --fqbn esp32:esp32:esp32 -t -v --input-dir build"
