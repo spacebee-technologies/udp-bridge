1c2d5606f97cdf2e1139ac999f7896aa3749bb06
sanntin <sanntinunez@gmail.com>
2023-07-13 11:47:44 -0300

    Async UDP

--- TITO_BRIDGE/TITO_BRIDGE.ino
+++ TITO_BRIDGE/TITO_BRIDGE.ino
@@ -1,9 +1,9 @@
 #include <WiFi.h>
-#include <ESPmDNS.h>
 #include <Wire.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <AsyncUDP.h>
 
 #include "circular_queue.h"
 #include "communication_sequence.h"
@@ -19,14 +19,18 @@ IPAddress gateway(192, 168, 1, 1);   // Dirección IP del gateway de la red
 IPAddress subnet(255, 255, 255, 0);  // Máscara de subred de la red
 IPAddress ip(192, 168, 1, 100); // Dirección IP del destinatario
 
+const char* udpServerIP = "192. 168.200.100";
+const int udpServerPort = 51524;
+const int port=51524;
 // This application listens for UDP packets from the network in the following port
-unsigned int port = 51524;
+
 
 CircularQueue circularQueue;
 CommunicationSequence communicationSequence;
 
 // Crea los objetos WiFi y UDP
 WiFiUDP udpHandler;
+AsyncUDP udp;
 
 void receiveEvent(int bytesReceived) {
   // Esta funcion se ejecuta siempre que el maestro envia un dato (master write)
@@ -47,7 +51,21 @@ void requestEvent() {
   communicationSequence.handleRequest(&circularQueue);
 }
 
+void processPacket(char* packet, int packetSize){
+    if (packetSize) {
+      //packetBuffer[packetSize]=0;
+      Serial.print("TC recibido: ");
+      for (int i = 0; i < packetSize; i++) {
+        Serial.print(packet[i], HEX);  // Print each byte in hexadecimal format
+        Serial.print(" ");
+      }
+      Serial.println();  // Print a new line after the hexadecimal output
+      circularQueue.enqueue(packet, packetSize);  // Agrego dato a FIFO
+    }
+  }
+
 void setup() {
+  delay(4000);
   Serial.begin(115200);
   Wire.begin(SLAVE_ADDRESS); // Inicializa la comunicación I2C como esclavo
   Wire.onReceive(receiveEvent); // Función que se llamará cuando se reciba un mensaje
@@ -62,19 +80,28 @@ void setup() {
   Serial.println("Conectado a la red WiFi.");
   Serial.print("Dirección IP asignada: ");
   Serial.println(WiFi.localIP());
+  Serial.print("Signal Strength: ");
+  Serial.println(WiFi.RSSI());
 
-  // Inicializa el servicio mDNS
-  if (!MDNS.begin("TITO")) {
-    Serial.println("Error al iniciar el servicio mDNS");
-  } else {
-    Serial.println("Servicio mDNS iniciado con éxito");
-    MDNS.addService("udp", "port", port);
-  }
 
-  // Inicializa los objetos UDP
-  udpHandler.begin(port);
-  Serial.print("UDP server started at port: ");
-  Serial.print(port);
+   if(udp.listen(port)) {
+      Serial.println("Escucho...");
+        udp.onPacket([](AsyncUDPPacket packet) {
+            int packetSize = packet.length();
+            char packetBuffer[packetSize];
+            memcpy(packetBuffer, (char*)packet.data(), packet.length());
+            packetBuffer[packetSize]=0;
+            Serial.print("TC recibido: ");
+            for (int i = 0; i < packetSize; i++) {
+              Serial.print(packetBuffer[i], HEX);  // Print each byte in hexadecimal format
+              Serial.print(" ");
+            }
+            circularQueue.enqueue(packetBuffer, packetSize); 
+            Serial.println(); 
+        });
+    }
+
+
 
   // Init queue
   circularQueue = CircularQueue();
@@ -83,20 +110,7 @@ void setup() {
   communicationSequence = CommunicationSequence();
 }
 
-void loop() {
-  // Espera a recibir datos en el puerto 3 para recibir TC
-  int packetSize = udpHandler.parsePacket();
-  if (packetSize) {
-    char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
-    udpHandler.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);  // Lee el mensaje recibido
-    packetBuffer[packetSize]=0;
-    Serial.print("TC recibido: ");
-    for (int i = 0; i < packetSize; i++) {
-      Serial.print(packetBuffer[i], HEX);  // Print each byte in hexadecimal format
-      Serial.print(" ");
-    }
-    Serial.println();  // Print a new line after the hexadecimal output
-    circularQueue.enqueue(packetBuffer, packetSize);  // Agrego dato a FIFO
-  }
+void loop(){
+
 }
 
